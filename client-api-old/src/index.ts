a789eb81529b4818bd04f3fc003ec9c4e0dc2750
hk-company-work <104336107+hk-company-work@users.noreply.github.com>
2022-05-28 06:04:40 +0800

    Add files via upload

---
+++ client-api-old/src/index.ts
@@ -0,0 +1,14 @@
+import		express	from "express";
+import * as	core	from "./core";
+
+function main() {
+	const app = express();
+	app.get("/", (req, res) => {
+		res.send("Hello from express and typescript");
+	});
+
+	const port = process.env.PORT || 3000;
+
+	app.listen(port, () => console.log("App listening on PORT ${port}"));
+};
+main();
